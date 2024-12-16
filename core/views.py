9d322e4a0fd35f751da5f015820a090fba03ac2f
Nworah-Gabriel <gabrielnworah6@gmail.com>
2024-12-16 06:36:32 +0000

    Created the project structure for fargin and worked on the landing page

---
+++ core/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib import messages
+from django.conf import settings
+import requests
+
+
+def index(request):
+    return render(request, "index.html")
